b63465bf67f47dcb9201286f6edaece4976cdae4
Tomasz Ferens <tomaszferens@hotmail.com>
2017-01-30 21:51:23 +0100

    inital commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import './styles/index.css';
+
+import store from './store';
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
